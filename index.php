88161d0b6495c36831af6efcc6acbbeca29babce
nedankinde <nedankinde03@gmail.com>
2025-05-16 21:58:36 -0400

    update

--- index.php
+++ index.php
@@ -6,16 +6,11 @@
 
     <!-- LIVE RELOADING -->
     <script src="http://localhost:35729/livereload.js"></script>
-
+    <link rel="stylesheet" href="src/style.css">
     <title>Website is Live!</title>
 </head>
 <body>
-
-<?php
-
-echo "Today's date is " . date("m/d/y");
-
-?>
-
+  <?php echo "Today's date is " . date("m/d/y"); ?>
+  <script src="src/script.js"></script>
 </body>
 </html>
---
+++ src/script.js
@@ -0,0 +1,1 @@
+alert("src/script.js says hello")
